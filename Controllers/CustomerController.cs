ee62f75c772807a0ad72b4ea309b688fad7274fc
roiveredritesh <36503631+roiveredritesh@users.noreply.github.com>
2024-05-14 18:22:17 +0530

    Add project files.

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,29 @@
+﻿using App.Application.Customer.Commands.CreateCustomer;
+using App.Application.Customer.Queries.GetCustomerById;
+using App.Application.Customer.Queries.GetCustomers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace App.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : APIControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Getallcustomers()
+        {
+            var customers = await Mediator.Send(new GetCustomersQuery());
+            return Ok(customers);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetcustomerbyId(string id)
+        {
+            var customer = await Mediator.Send(new GetCustomerByIdQuery { Id = id });
+            return Ok(customer);
+        }
+
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+using App.Application;
+using App.Infra;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+// Add layers dependencies
+builder.Services.AddApplicationServices();
+builder.Services.AddInfrastructureServices(builder.Configuration);
+
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
